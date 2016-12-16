7d22f56c30c83522c1097f34a537357d971aff2a
srihandayanii <srihhandayanii@gmail.com>
2016-12-16 20:46:14 +0700

    Update Model
    update model and add Profile

--- app/Post.php
+++ app/Post.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
      protected $fillable = [
-        'title','content','like','dislike','user_id','category_id','types_id'
+        'title','content','user_id','category_id','types_id'
     ];
 
     public function user(){
